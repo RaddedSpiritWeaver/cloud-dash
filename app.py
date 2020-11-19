0a84e88e6cf7072ca323e1412cf501408229bfe0
Erfan ALizadeh Noohi <erfan.noohi@gmail.com>
2020-11-19 15:11:17 +0330

    added modify and working clone

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+import time
 import vboxapi
 import virtualbox
 import os
+from datetime import datetime
+import subprocess
 
 app = FastAPI()
 vbox = virtualbox.VirtualBox()
@@ -26,6 +29,7 @@ async def command():
 
 @app.get("/power")
 def stop_start(stop: bool, vm_name: str):
+    vm_name = '\'{}\''.format(vm_name)
     # todo: start and stop vms and keep track of their sessions soo you can interact with them
     stream = None
     if stop:
@@ -41,6 +45,7 @@ def stop_start(stop: bool, vm_name: str):
 
 @app.get("/status")
 def status(vm_name: str):
+    # vm_name = '\'{}\''.format(vm_name)
     machine = vbox.find_machine(vm_name)
     return machine.state
 
@@ -53,14 +58,35 @@ def status(vm_name: str):
 @app.get("/deep")
 def depInf(vm_name: str):
     # todo: idk how to use the main api for this
+    vm_name = '\'{}\''.format(vm_name)
     stream = os.popen(v_command + 'showvminfo ' + vm_name)
     return stream.readlines()
 # retrons a list of strings that, a sample output is provided
 
-def modify(): pass
+@app.get("/modify")
+def modify(vm_name: str, cpus: int = None, memory: int = None):
+    # vm_name = '\'{}\''.format(vm_name)
+    process = subprocess.Popen(["vboxmanage", 'modifyvm', vm_name, "--cpus", str(cpus), "--memory", str(memory)])
+    stdout, stderr = process.communicate()
+    return None
 
 def delete_vm(): pass
 
-def clone_vm(): pass
+@app.get("/clone")
+def clone_vm(vm_name: str):
+    old_dir = os.getcwd()
+    os.chdir("/home/spiritweaver/VirtualBox VMs/")
+    base_store = "clones/"
+    current_time = datetime.now()
+    name = "{}{}_{}.ova".format(base_store, vm_name, current_time)
+    # name = 
+    process = subprocess.Popen(['vboxmanage', 'export', vm_name, '-o', name])
+    stdout, stderr = process.communicate()
+    print("starting wait")
+    time.sleep(5)
+    os.popen(v_command + "import " + '\''+ name + '\'')
+    os.chdir(old_dir)
+    return name
+    
 
 def pass_command(): pass
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import virtualbox
+import vboxapi
 
 vbox = virtualbox.VirtualBox()
+vbox_manager = vboxapi.VirtualBoxManager
 
 session = virtualbox.Session()
 machine = vbox.find_machine("vm2")
-print(machine.q)
